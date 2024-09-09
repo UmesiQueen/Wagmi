a971a275b1f030d0dc6115745e68d34e7a684539
Queen Umesi <queenumesi01@gmail.com>
2024-09-09 15:16:44 +0100

    chore: Connect wallet with WC

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
-
 function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
-
-  return (
-    <>
-      <div>
-        <h2>Account</h2>
-
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
-
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
-      </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+	return (
+		<div className="p-10">
+			<div className="flex justify-between items-center">
+				<h2 className="text-2xl font-semibold">My Wallet</h2>
+				<w3m-button />
+			</div>
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,19 +1,44 @@
-import { http, createConfig } from 'wagmi'
-import { mainnet, sepolia } from 'wagmi/chains'
+import { http } from 'wagmi'
+import { mainnet, sepolia, base } from 'wagmi/chains'
 import { coinbaseWallet, injected, walletConnect } from 'wagmi/connectors'
 
-export const config = createConfig({
-  chains: [mainnet, sepolia],
+import { createWeb3Modal } from '@web3modal/wagmi/react'
+import { defaultWagmiConfig } from '@web3modal/wagmi/react/config'
+
+
+const projectId = import.meta.env.VITE_WC_PROJECT_ID;
+
+// 2. Create wagmiConfig
+const metadata = {
+  name: 'AppKit',
+  description: 'AppKit Example',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/37784886']
+}
+
+export const config = defaultWagmiConfig({
+  chains: [mainnet, sepolia, base],
   connectors: [
     injected(),
     coinbaseWallet(),
-    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+    walletConnect({ projectId }),
   ],
   transports: {
     [mainnet.id]: http(),
     [sepolia.id]: http(),
+    [base.id]: http(),
   },
+  metadata
 })
+
+// 3. Create modal
+createWeb3Modal({
+  metadata,
+  wagmiConfig: config,
+  projectId,
+  enableAnalytics: true // Optional - defaults to your Cloud configuration
+})
+
 
 declare module 'wagmi' {
   interface Register {
